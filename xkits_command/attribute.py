7dc2a5ced94d8d5db991bd8db3d6e744c76ce1c2
Mingzhe Zou <zoumingzhe@qq.com>
2025-05-28 12:42:24 +0800

    bump version to 0.4.2

--- xkits_command/attribute.py
+++ xkits_command/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xkits-command"
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __urlhome__ = "https://github.com/bondbox/xcommand/"
 __description__ = "Command line module"
 
