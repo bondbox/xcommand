73c499a4aa3a545d4b38f7838a40c3dffe7c96b7
Mingzhe Zou <zoumingzhe@qq.com>
2026-06-15 02:22:06 +0800

    build: remove unused xpip-build dependency from pyproject.toml

--- xkits_command/attribute.py
+++ xkits_command/attribute.py
@@ -1,8 +1,11 @@
 # coding:utf-8
 
-__version__ = "0.6"
-
 # project info
 __project_name__ = "xcommand"
 __project_home__ = "https://github.com/bondbox/xcommand/"
 __project_desc__ = "Command line module"
+
+# package info
+__version__ = "0.6"
+__package_name__ = "xkits-command"
+__package_desc__ = "Command line module"
